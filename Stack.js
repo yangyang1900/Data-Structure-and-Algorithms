5364356b2ad32c973201e99a363c0adf0423cc86
yangyang <2440286217@qq.com>
2019-08-21 19:43:29 +0800

    Stack.js

--- Stack.js
+++ Stack.js
@@ -2,7 +2,7 @@
 * @Author: yangyang
 * @Date:   2019-08-21 16:38:54
 * @Last Modified by:   yangyang
-* @Last Modified time: 2019-08-21 16:51:49
+* @Last Modified time: 2019-08-21 19:43:18
 */
 
 /*
@@ -52,6 +52,38 @@ class Stack{
 		this.items.push(element)
 		this.count++
 	}
-
-	size
+	size(){
+		return this.count
+	}
+	isEmpty(){
+		return this.count === 0;
+	}
+	pop(){
+		if(this.isEmpty()){
+			return undefined
+		}
+		const result = this.items[this.count]  //取出pop掉的值
+		delete this.items[this.count]
+		return result						//返回删掉的值
+	}
+	peek(){
+		if(this.isEmpty()){
+			return undefined
+		}
+		return this.items[this.count - 1]
+	}
+	clear(){
+		this.items = {}
+		this.count = 0
+	}
+	//toString方法
+	toString(){
+		if(this.isEmpty){
+			return ''
+		}
+		let objString = `${this.items[0]}`		//类似sum=sum+arr[i]，需要个初始值sum
+		for(let i = 1;i < this.count;i++){
+			objString = `${objString},${this.items[i]}`; //类似sum = sum +arr[i]
+		}
+	}
 }
