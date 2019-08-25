55e1ca4159c37aec30fc92ebd8601d823a436bee
yangyang <2440286217@qq.com>
2019-08-25 14:11:35 +0800

    链表-排序链表

--- 链表-排序链表.js
+++ 链表-排序链表.js
@@ -2,7 +2,7 @@
 * @Author: yangyang
 * @Date:   2019-08-22 23:04:57
 * @Last Modified by:   yangyang
-* @Last Modified time: 2019-08-25 08:34:00
+* @Last Modified time: 2019-08-25 14:10:50
 */
 
 /*
@@ -61,17 +61,31 @@ let swap = (p,q) =>{
 
 //寻找基准元素的节点
 let partion = (begin,end) =>{
-	let val = begin.val
-	let p = begin
-	let q = begin.next
+	
+	let val = begin.val   //起初的基准元素
+	let p = begin		//初始化的p指针
+	let q = begin.next	//初始化的q指针
+
 	while(q !== end){
 		if(q.val < val){
-			p = p.next
-			swap(p,q)
+			p = p.next	//因为p指针左侧都是小于基准点，所以当发现q.val小于基准点val的值时，是此时p指针.next与q指针交换
+			swap(p,q)	//为什么要p=p.next，因为要给把发现小于val的q值换过来，所以申请了一个位置p的下一个位置p.next
 		}
-		q = q.next
+		q = q.next //q交换后，继续检查下一个元素q.next
 	}
-	//让基准元素跑到中间去
+	//让基准元素跑到中间去（当我们检查第一遍之后，能保证p指针左侧及p指针小于基准值，p到q指针及q指针区间，大于基准值，此时我们就可以变换基准值的位置了）
 	swap(p,begin)
+
+	//返回基准元素p
 	return p
+}
+
+//排序过程
+let sort = (begin,end) =>{
+
+	if(begin != end){
+		let part = partion(begin,end)	//寻找基准点，基准点左侧是一部分，右侧是一部分
+		sort(begin,part)	//递归
+		sort(part.next,end)	//递归
+	}
 }
